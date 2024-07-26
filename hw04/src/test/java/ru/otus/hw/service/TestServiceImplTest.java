8dd4222b1aae2d17fda8f36ce82307904c768543
aleksey_andris <aleksejandris51@gmail.com>
2024-07-26 19:19:47 +0300

    corrected design

--- hw04/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw04/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest()
+@SpringBootTest
 class TestServiceImplTest {
 
     @MockBean
