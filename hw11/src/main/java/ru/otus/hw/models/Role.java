d922a474ec70ff9ebbd445830b8d751e6bdbebb1
aleksey_andris <aleksejandris51@gmail.com>
2024-10-06 11:15:01 +0300

    role not enum

--- hw11/src/main/java/ru/otus/hw/models/Role.java
+++ hw11/src/main/java/ru/otus/hw/models/Role.java
@@ -2,8 +2,6 @@ package ru.otus.hw.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,8 +21,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Enumerated(value = EnumType.STRING)
     @Column(name = "name")
-    private RoleName name;
+    private  String name;
 
 }
--- hw11/src/main/java/ru/otus/hw/models/RoleName.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw.models;
-
-public enum RoleName {
-    USER,
-    MODERATOR,
-    ADMIN
-}
